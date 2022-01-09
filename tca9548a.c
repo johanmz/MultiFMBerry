34b80974a41be831c6e0e62c65f4536246414a03
JohanM <johndoe@example.com>
2022-01-09 21:55:56 +0100

    initialisation routines for multiplexers

---
+++ tca9548a.c
@@ -0,0 +1,43 @@
+/*
+	FMBerry - an cheap and easy way of transmitting music with your Pi.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <stdio.h>
+#include "i2c.h"
+#include "defs.h"
+#include "fmberryd.h"
+
+
+// I2C address of tca9548a multiplexer
+static int address;
+
+// initialise i2c bus for tca9548a multiplexer
+int tca9548a_init_i2c (uint8_t bus)
+{
+    for (int j=0; j<MAXMULTIPLEXERS; j++)
+    {
+        address = multiplexer[j].address;
+        if (address)
+        {
+            multiplexer[j].i2cbus=-1;
+            multiplexer[j].i2cbus=i2c_init(bus, address);
+            if ((multiplexer[j].i2cbus == -1) || (i2c_send(multiplexer[j].i2cbus, 0x00, 0x00) == -1))
+                return -1;
+        }
+        else   
+            break;
+    }
+    return 0;
+}
